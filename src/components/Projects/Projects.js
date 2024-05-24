16f681add44e3a290f05c3a775d3f7c0bb6c9316
nimitagarwal26 <agarwalnimit88@gmail.com>
2024-05-24 14:31:03 +0530

    Updated: Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import StaticCodeAnalyser from "../../Assets/Projects/StaticCodeAnalyser.png";
+import bus from "../../Assets/Projects/bus.png";
 import CryptoHunter from "../../Assets/Projects/CryptoHunter.png";
+import ttt from "../../Assets/Projects/ttt.png";
 
 function Projects() {
   return (
@@ -31,11 +32,21 @@ function Projects() {
 
         <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={StaticCodeAnalyser}
+              imgPath={bus}
               isBlog={false}
-              title="Static Code Analyser"
-              description="The Static Code Analyzer is a robust tool designed to analyze Java source code statically, identifying potential issues, violations of coding conventions, and design flaws. It dynamically processes XML-based rules for flexible rule definitions and customization. The tool integrates a perceptron-based AI model to evaluate code quality, detect patterns, and identify areas for improvement. Additionally, it features a comment density checker to ensure the maintainability and clarity of the codebase by evaluating the adequacy of code comments."
-              ghLink="https://github.com/K144U/--static_code_analysis_ISRO_respondbasket"
+              title="Bus Booking System"
+              description="The bus booking project, built using Python, simplifies the ticket reservation process with features like seat selection, payment processing, and scheduling. It leverages real-time data to manage bookings, cancellations, and availability efficiently. The system ensures a seamless experience for users by providing intuitive interfaces, while administrators benefit from streamlined operations and robust management tools. This project aims to enhance the overall efficiency and user satisfaction in bus travel reservations."
+              ghLink="https://github.com/nimitagarwal26/Bus-Booking-System"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ttt}
+              isBlog={false}
+              title="Tic Tac Toe"
+              description="The Tic Tac Toe project is a classic game developed to provide engaging entertainment. It allows two players to compete on a 3x3 grid, aiming to align three marks in a row, column, or diagonal. With an intuitive interface, the game ensures a seamless user experience, offering both fun and mental stimulation. Ideal for quick play sessions, it helps improve strategic thinking and decision-making skills."
+              ghLink="https://github.com/nimitagarwal26/TicTacToe"
             />
           </Col>
 
